23b67a09282fe2f2323e965dc82f8c5180735ef0
3116005131 <duanhaobin@icloud.com>
2018-09-07 16:38:05 +0800

    尝试一下如何上传到github。

---
+++ Word_Counter/Word_Counter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\textEng.txt";
+            StreamReader reader;
+            reader = new StreamReader(path, System.Text.Encoding.Default);
+            reader.BaseStream.Seek(10, SeekOrigin.Begin);
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine("{0}", line);
+                line = reader.ReadLine();
+            }
+            reader.Close();
+            Console.ReadLine();
+        }
+    }
+}
