cd2f6e4eb0b5cd81b1a793c09f026b8c3bba770a
3116005131 <duanhaobin@icloud.com>
2018-09-07 18:08:36 +0800

    练习用C#编写WC

--- Word_Counter/Word_Counter/Program.cs
+++ Word_Counter/Word_Counter/Program.cs
@@ -11,18 +11,13 @@ namespace Word_Counter
     {
         static void Main(string[] args)
         {
-            string path = @"D:\textEng.txt";
-            StreamReader reader;
-            reader = new StreamReader(path, System.Text.Encoding.Default);
-            reader.BaseStream.Seek(10, SeekOrigin.Begin);
-            string line = reader.ReadLine();
-            while (line != null)
-            {
-                Console.WriteLine("{0}", line);
-                line = reader.ReadLine();
-            }
-            reader.Close();
-            Console.ReadLine();
+            string path = @"d:\text.dat";
+            FileStream fs = new FileStream(path, FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryWriter writer = new BinaryWriter(fs);//其中，参数output 用于设置流对象，通常是由FileStream 类实例化的对象。
+            writer.BaseStream.Seek(0, SeekOrigin.Begin);
+            writer.Write("中华人民共和国");
+            writer.Close();
+            fs.Close();
         }
     }
 }
