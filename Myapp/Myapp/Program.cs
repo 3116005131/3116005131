68f6e6d8ab8880bde5d1f9ec73bcdcfedc0fd112
3116005131 <duanhaobin@icloud.com>
2018-09-26 17:34:30 +0800

    四则运算

--- Myapp/Myapp/Program.cs
+++ Myapp/Myapp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,9 @@ namespace Myapp
     {
         public class TreeNode
         {
-            string data;
-            TreeNode left;
-            TreeNode right;
+            public string data;
+            public TreeNode left;
+            public TreeNode right;
 
             public TreeNode()
             {
@@ -72,8 +73,8 @@ namespace Myapp
                 }
                 char[] exp = expression.ToCharArray();
                 TreeNode newNode = new TreeNode();
-                string lefrString;
-                string rightString;
+                string lefrString=null;
+                string rightString=null;
                 string stack=null;       //用于存储括号的栈  当表达式检测完毕后 若栈部位空  这说明括号的使用不合法
                 if (checkOperator(expression)==true)
                 {
@@ -118,9 +119,358 @@ namespace Myapp
                     {   //说明表达式的最后运算为加法或减法
                         separator = index;
                     }
+                    else
+                    {
+                        separator = multi_div;
+                    }
+                    newNode.data = Convert.ToString(exp[separator]);
+                    int pos = 0;
+                    for (; pos < separator; pos++)
+                    {
+                        lefrString = lefrString + exp[pos];
+                    }
+                    pos++;
+                    for (; pos < separator; pos++)
+                    {
+                        rightString = rightString + exp[pos];
+                    }
+                    if (node == null)
+                    {
+                        node = newNode;
+                    }
+                    newNode.left = createTree(lefrString);
+                    newNode.right = createTree(rightString);
+                }
+                else
+                {
+                    newNode.data = expression;
                 }
                 return newNode;
             }
+
+            public void posOrder(TreeNode currentNode)
+            {
+                if (currentNode != null)
+                {
+                    posOrder(currentNode.left);
+                    posOrder(currentNode.right);
+                    Console.Write(currentNode.data);
+                }
+            }
+        }
+        public class FenShu
+        {
+            private int fenzi;//分子
+            private int fenmu;//分母
+            public int attFenzi //属性
+            {
+                get { return fenzi; }
+                set { fenzi = value; }
+            }
+            public int attFenmu
+            {
+                get { return fenmu; }
+                set { fenmu = value; }
+            }
+            public FenShu() { }
+            public FenShu (int molecular,int assignment)
+            {
+                fenzi = molecular;
+                fenmu = assignment;
+            }
+            public FenShu simplify(FenShu a)
+            {
+                int small;//记录相当小的数
+                int big;//记录相当大的数
+                if (a.fenzi >a.fenmu)
+                {
+                    small = a.fenmu;
+                    big = a.fenzi;
+                }
+                else
+                {
+                    small = a.fenzi;
+                    big = a.fenmu;
+                }
+                //化简 例如：99/22=>9/2
+                int maxCommonFactor = 1;
+                for (int i = 2; i <=small/2; i++)
+                {
+                    if (small%i==0&&big%i==0)
+                    {
+                        maxCommonFactor = i;
+                    }
+                }
+                a.fenzi /= maxCommonFactor;
+                a.fenmu /= maxCommonFactor;
+                return a;
+            }
+                //运算符重载
+            public static FenShu operator +(FenShu a,FenShu b)
+            {
+                a.fenzi = a.fenzi * b.fenmu + b.fenzi * a.fenmu;
+                a.fenmu = a.fenmu * b.fenmu;
+                a = a.simplify(a);
+                return a;
+            }
+            public static FenShu operator -(FenShu a, FenShu b)
+            {
+                a.fenzi = a.fenzi * b.fenmu - b.fenzi * a.fenmu;
+                a.fenmu = a.fenmu * b.fenmu;
+                a = a.simplify(a);
+                return a;
+            }
+            public static FenShu operator *(FenShu a, FenShu b)
+            {
+                a.fenzi = a.fenzi * b.fenzi;
+                a.fenmu = a.fenmu * b.fenmu;
+                a = a.simplify(a);
+                return a;
+            }
+            public static FenShu operator /(FenShu a, FenShu b)
+            {
+                a.fenzi = a.fenzi * b.fenmu;
+                a.fenmu = a.fenmu * b.fenzi;
+                a = a.simplify(a);
+                return a;
+            }
+            public FenShu StrToFrc(string str)
+            {
+                FenShu a = new FenShu();
+                string[] num = str.Split('/');
+                a.fenzi = Convert.ToInt32(num[0]);
+                a.fenmu = Convert.ToInt32(num[1]);
+                return a;
+            }
+            public string FrcToStr(FenShu a)
+            {
+                string str = "";
+                if (a.fenzi==0)
+                {
+                    str = 0 + "";
+                }
+                else
+                {
+                    int x = a.fenzi;
+                    int y = a.fenmu;
+                    int flag = 0;
+                    if (x < 0) flag += 1;
+                    x = Math.Abs(x);
+                    if (y < 0) flag += 1;
+                    y = Math.Abs(y);
+                    if (flag == 1) str += "-";
+                    if (x >= y)
+                    {
+                        int z = x / y;
+                        int q = x % y;
+                        if (q == 0)
+                        {
+                            str += z + "";
+                        }
+                        else
+                        {
+                            str += z + "" + "^" + q + "" + "/" + y + "";
+                        }
+                    }
+                    else
+                    {
+                        str += x + "" + "/" + y + "";
+                    }
+                }
+                return str;
+            }
+        }
+        public class Calculate
+        {
+            private FenShu answer;
+            private List<FenShu> answersList;
+            private List<TreeNode> treeList;
+
+            public FenShu getA
+        }
+        public class Create
+        {
+            private int range;
+            private int difcoe;
+            private int problemNum;
+            private string problem;
+            private List<string> problemList;
+
+            public int attRange
+            {
+                get { return range; }
+                set { range = value; }
+            }
+            public int attDifcoe
+            {
+                get { return difcoe; }
+                set { difcoe = value; }
+            }
+            public int attProblemNum
+            {
+                get { return problemNum; }
+                set {problemNum = value; }
+            }
+            public string  attProblem
+            {
+                get { return problem; }
+                set { problem = value; }
+            }
+            public List<string> attProblemList
+            {
+                get { return problemList; }
+                set { problemList = value; }
+            }
+            public string Createnum(int range)
+            {
+                Random rand = new Random();
+                int ran = rand.Next(1, range-1);
+                int num = 1, num2 = 1;
+                string s = null;
+                if (ran < range / 2)
+                {
+                    num = rand.Next(1, range - 1);
+                    s = Convert.ToString(num);
+                }
+                else
+                {
+                    num = rand.Next(1, range);
+                    num2 = rand.Next(1, range);
+                    s = "(" + Convert.ToString(num) + "/" + Convert.ToString(num2) + ")";
+                }
+                return s;
+            }
+            public string CreatArith()
+            {
+                string s = null;
+                Random rand = new Random();
+                int ran = rand.Next(1, 4);
+                switch (ran)
+                {
+                    case 1:
+                        s = "+";
+                        break;
+                    case 2:
+                        s = "-";
+                        break;
+                    case 3:
+                        s = "*";
+                        break;
+                    case 4:
+                        s = "/";
+                        break;
+                    default:
+                        break;
+                }
+                return s;
+            }
+            public string createProblem(int range)
+            {
+                Random r = new Random;
+                string s="";
+                string tempNum = null;
+                string tempOperator = null;
+                int operatorNum = r.Next(2, 4);
+                int bracket_left = 0;
+                int bracket_right = 0;
+                bracket_left = r.Next(1, 4);
+                if (bracket_left == 1)
+                {
+                    s += "(";
+                    bracket_right++;
+                }
+                s += "" + r.Next(range);
+                for (int j = 0; j < operatorNum; j++)
+                {
+
+                }
+                return s;
+            }
+
+        }
+        public class GetExp
+        {
+            public double e;
+            public bool flag = false;
+            public double getExpression(TreeNode currNode)
+            {
+                if(currNode.left==null||currNode.right==null)
+                {
+                    string str = currNode.data;
+                    e = Convert.ToInt32(str.Trim());
+                    return e;
+                }
+                else
+                {
+                    if (currNode.data.Equals("+"))
+                    {
+                        e = getExpression(currNode.left) + getExpression(currNode.right);
+                    }
+                    if (currNode.data.Equals("-"))
+                    {
+                        e = getExpression(currNode.left) - getExpression(currNode.right);
+                    }
+                    if (currNode.data.Equals("*"))
+                    {
+                        e = getExpression(currNode.left) * getExpression(currNode.right);
+                    }
+                    if (currNode.data.Equals("/"))
+                    {
+                        if(getExpression(currNode.right )!=0)
+                        e = getExpression(currNode.left) / getExpression(currNode.right);
+                        else { flag = true; }
+                    }
+
+                }
+                return e;
+            }
+        }
+        public class Result
+        {
+            private List<TreeNode> treeList;
+            private List<string> problemList;
+            private string right;
+            private string wrong;
+
+            public string attRight
+            {
+                get { return right; }
+                set { right = value; }
+            }
+            public string attWrong
+            {
+                get { return wrong; }
+                set { wrong = value; }
+            }
+            public List<TreeNode> attTreeList
+            {
+                get { return treeList; }
+                set { treeList = value; }
+            }
+            public Result(List<TreeNode> treeList,List<string> problemList)
+            {
+                this.treeList = treeList;
+                this.problemList = problemList;
+                right = null;
+                wrong = null;
+            }
+            public void CompareAnswer(String str)
+            {
+                try
+                {
+                    
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+
+        }
+        public class Total
+        {
+
         }
         static void Main(string[] args)
         {
